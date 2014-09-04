420725e362141551ee2280c650fa4579fee788ff
Guido Grassel <guido@guidos-mbp.grassel.org>
2014-09-04 22:32:39 +0300

    working according to tutorial at https://www.youtube.com/watch?v=uB100xVS_Yc

--- UlmMunsterApp/ViewController.swift
+++ UlmMunsterApp/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import MapKit;
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
                             
+    @IBOutlet weak var theMapView: MKMapView!;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
+        var lat : CLLocationDegrees = 48.399193;
+        var long : CLLocationDegrees = 9.993341;
+    
+        var latDelta : CLLocationDegrees = 0.01;
+        var longDelta : CLLocationDegrees = 0.01;
+        
+        var theSpan : MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta);
+        
+        var churchLocation : CLLocationCoordinate2D = CLLocationCoordinate2DMake(lat, long);
+        var theRegion : MKCoordinateRegion = MKCoordinateRegionMake(churchLocation, theSpan);
+        
+        self.theMapView.setRegion(theRegion, animated: true);
+        
+        var theUlmMunsterAnnotation = MKPointAnnotation();
+        theUlmMunsterAnnotation.coordinate = churchLocation;
+        theUlmMunsterAnnotation.title = "Ulm Munster";
+        theUlmMunsterAnnotation.subtitle = "Hochste Kirche"
+        self.theMapView.addAnnotation(theUlmMunsterAnnotation);
     }
 
     override func didReceiveMemoryWarning() {
